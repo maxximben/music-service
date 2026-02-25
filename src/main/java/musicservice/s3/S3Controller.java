238851af54d032657015497495eac2fb29943b5c
Maksim <maximben888@gmail.com>
2026-02-25 11:35:55 +0600

    add s3 config

--- src/main/java/musicservice/s3/S3Controller.java
+++ src/main/java/musicservice/s3/S3Controller.java
@@ -1,4 +1,54 @@
 package musicservice.s3;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class S3Controller {
+
+    @Autowired
+    private S3Client s3Client;
+
+    @GetMapping("/api/files")
+    public ResponseEntity<List<FileInfo>> listFiles(
+            @RequestParam String bucket,
+            @RequestParam(required = false) String prefix,
+            @RequestParam(required = false, defaultValue = "1000") int maxKeys
+    ) {
+        ListObjectsV2Request request = ListObjectsV2Request.builder()
+                .bucket(bucket)
+                .prefix(prefix != null ? prefix : "")
+                .maxKeys(maxKeys)
+                .build();
+
+        ListObjectsV2Response response = s3Client.listObjectsV2(request);
+
+        List<FileInfo> files = new ArrayList<>();
+
+        for (var obj : response.contents()) {
+            String key = obj.key();
+            files.add(new FileInfo(key, getFileName(key)));
+        }
+
+        return ResponseEntity.ok(files);
+    }
+
+    private String getFileName(String key) {
+        int lastSlash = key.lastIndexOf('/');
+        return lastSlash >= 0 ? key.substring(lastSlash + 1) : key;
+    }
 }
+
+record FileInfo(
+        String key,
+        String fileName
+) {}
