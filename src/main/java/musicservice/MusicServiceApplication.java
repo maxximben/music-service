d823223567bb62492db84ff16addabf26334dc84
Maksim <maximben888@gmail.com>
2026-02-25 10:58:08 +0600

    rename package

--- src/main/java/musicservice/MusicServiceApplication.java
+++ src/main/java/musicservice/MusicServiceApplication.java
@@ -1,4 +1,4 @@
-package musicservice.musicservice;
+package musicservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/musicservice/MusicServiceApplicationTests.java
+++ src/test/java/musicservice/MusicServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package musicservice.musicservice;
+package musicservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
