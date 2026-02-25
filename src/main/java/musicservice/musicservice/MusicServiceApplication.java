c7522be93c744de1828cb495cedc92a9ae42742b
Maksim <maximben888@gmail.com>
2026-02-25 10:54:55 +0600

    add project

---
+++ src/main/java/musicservice/musicservice/MusicServiceApplication.java
@@ -0,0 +1,13 @@
+package musicservice.musicservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(MusicServiceApplication.class, args);
+    }
+
+}
