85a7f68083df51d3156d0ae87fcb580044ea7720
Maksim <maximben888@gmail.com>
2026-04-23 23:59:26 +0600

    update

--- src/main/java/musicservice/library/LibraryController.java
+++ src/main/java/musicservice/library/LibraryController.java
@@ -1,9 +1,10 @@
 package musicservice.library;
 
+import musicservice.account.AccountRepository;
+import musicservice.jwt.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +13,18 @@ public class LibraryController {
     @Autowired
     LibraryRepository libraryRepository;
 
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
     @GetMapping("/library")
-    public Library getLibrary(@RequestParam int userId,  @RequestHeader(value = "Authorization") String authHeader) {
+    public Library getLibrary(@RequestHeader(value = "Authorization") String authHeader) {
+        int userId = accountRepository.getIdByEmail(
+                jwtUtils.getEmailFromToken(authHeader.substring(7))
+        );
+
         return libraryRepository.findItemsByUserId(userId);
     }
 }
